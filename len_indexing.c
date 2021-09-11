f5eeb908972a2f6db0c6159fb31fbd3b8dc9f5d4
ahorsedesignedbyacommittee <80117449+ahorsedesignedbyacommittee@users.noreply.github.com>
2021-09-11 21:28:53 +0200

    Update len_indexing.c

--- len_indexing.c
+++ len_indexing.c
@@ -1,1 +1,11 @@
-
+int len(int n)
+{
+    int d = 0;
+    int r = n;   
+    while (r != 0) //Loops through the number with division by 10 until this division leaves no result > 10
+    {
+        r /= 10;
+        d++;
+    }
+    return d;
+}
